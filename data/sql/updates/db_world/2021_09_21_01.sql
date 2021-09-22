507a7867e2ba2aea240752bcdcf2839a81c856c6
arkane21 <arkane211@gmail.com>
2021-09-22 17:27:09 +0200

    fix (DB/Ulduar) Freya adds, correccion

--- data/sql/updates/db_world/2021_09_21_01.sql
+++ data/sql/updates/db_world/2021_09_21_01.sql
@@ -1,5 +1,4 @@
 -- Inmune mask add Freya --
 
-UPDATE creature_template SET mechanic_immune_mask=617299803 WHERE entry IN (32916,33400);
+UPDATE creature_template SET mechanic_immune_mask=617294619 WHERE entry IN (32916,33400,32918,33399);
 
--- ANTIGUO: 617298715 --
