79be09ef8c9c44534d5d52289136e9d3f1a95992
arkane21 <arkane211@gmail.com>
2021-09-22 17:38:44 +0200

    Coreccion adds Freya

--- data/sql/updates/db_world/2021_09_21_01.sql
+++ data/sql/updates/db_world/2021_09_21_01.sql
@@ -1,4 +1,4 @@
 -- Inmune mask add Freya --
 
 UPDATE creature_template SET mechanic_immune_mask=617294619 WHERE entry IN (32916,33400,32918,33399);
-
+UPDATE creature_template SET mechanic_immune_mask=mechanic_immune_mask &~2048 WHERE entry IN (32918,33399);
