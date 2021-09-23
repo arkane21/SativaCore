b031649ddbd220f36e11bd39a8964b8c4c55ea90
arkane21 <arkane211@gmail.com>
2021-09-23 16:35:25 +0200

    fix(DB/Ulduar) Correct damage on adds in 25m (Deez Work)

---
+++ data/sql/updates/db_world/2021_09_23_00.sql
@@ -0,0 +1,34 @@
+-- Ancient Water Spirit
+UPDATE creature_template SET mindmg=15174, maxdmg=20778 WHERE entry IN (33398); 
+
+-- Storm Lasher
+UPDATE creature_template SET mindmg=15174, maxdmg=20778 WHERE entry IN (33401); 
+
+-- Snaplasher 
+UPDATE creature_template SET mindmg=15174, maxdmg=20778 WHERE entry IN (33400); 
+
+-- Freya
+UPDATE creature_template SET mindmg=30000, maxdmg=43000 WHERE entry IN (33360);
+
+-- Ancestro 
+UPDATE creature_template SET mindmg=25020, maxdmg=33020 WHERE entry IN (33376);
+
+-- Plantas esplosivas 
+UPDATE creature_template SET mindmg=1250, maxdmg=17050 WHERE entry IN (33399);
+
+-- Thorim Adds de adentro los pequeños 
+UPDATE creature_template SET mindmg=3480, maxdmg=4530 WHERE entry IN (33161); 
+UPDATE creature_template SET mindmg=3480, maxdmg=4530 WHERE entry IN (33162);
+-- Oleada de afuera 
+UPDATE creature_template SET mindmg=3200, maxdmg=3500 WHERE entry IN (33157); 
+UPDATE creature_template SET mindmg=11310, maxdmg=15420 WHERE entry IN (33158); 
+UPDATE creature_template SET mindmg=11310, maxdmg=15420 WHERE entry IN (33155); 
+
+-- Thorim 
+UPDATE creature_template SET mindmg=14050, maxdmg=18500 WHERE entry IN (33147); 
+
+-- Hodir
+UPDATE creature_template SET mindmg=40900, maxdmg=52900 WHERE entry IN (32846); 
+
+-- Mimiron 
+UPDATE creature_template SET mindmg=32050, maxdmg=43584 WHERE entry IN (34106);
