aa27de05c26ba53d3546c0fdc9a04284882a8b46
iyasenai <jh1160@hotmail.com>
2021-09-24 16:47:51 -0500

    fix(Core/DB/Ulduar) Correcion al InmuneMask en el encuentro contra Freya

---
+++ data/sql/updates/db_world/2021_09_24_00.sql
@@ -0,0 +1,6 @@
+-- Adds freya
+
+UPDATE creature_template SET mechanic_immune_mask=579811199 WHERE entry IN (33202,33398); 
+UPDATE creature_template SET mechanic_immune_mask=617292571 WHERE entry IN (32916,33400); 
+UPDATE creature_template SET mechanic_immune_mask=617297755 WHERE entry IN (32919,33401); 
+UPDATE creature_template SET mechanic_immune_mask=617292571 WHERE entry IN (32918,33399);
