0fd9a5223eadc3ce3c1095832391e9149b6931d9
arkane21 <arkane211@gmail.com>
2021-09-25 21:18:37 +0200

    fix(DB/Ulduar) Damages in 10m (Deez work)

---
+++ data/sql/updates/db_world/2021_09_25_00.sql
@@ -0,0 +1,38 @@
+-- General vezax --
+UPDATE creature_template SET mindmg=41900, maxdmg=53900 WHERE entry IN (33449);
+-- Ignis maestro de la caldera  --
+UPDATE creature_template SET mindmg=40900, maxdmg=50400 WHERE entry IN (33190);
+-- Adds de tajo escama --
+-- vigia runa oscura
+UPDATE creature_template SET mindmg=12600, maxdmg=17496 WHERE entry IN (33851); 
+-- guardian runa oscura
+UPDATE creature_template SET mindmg=16210, maxdmg=22860 WHERE entry IN (33850);
+-- Tajo escama
+UPDATE creature_template SET mindmg=40900, maxdmg=52900 WHERE entry IN (33724);
+-- XT -- 
+UPDATE creature_template SET mindmg=40900, maxdmg=52900 WHERE entry IN (33885);
+-- kologarn -- 
+UPDATE creature_template SET mindmg=40900, maxdmg=52900 WHERE entry IN (33909);
+-- "Campeon de Hodir" --
+UPDATE creature_template SET mindmg=2000, maxdmg=2600 WHERE entry IN (34139);
+UPDATE creature_template SET mindmg=2200, maxdmg=3200 WHERE entry IN (34133);
+-- Elementales de Hielo: --
+-- "Aparecido infernal" --
+UPDATE creature_template SET mindmg=1350, maxdmg=2500 WHERE entry IN (34134); 
+UPDATE creature_template SET mindmg=1350, maxdmg=2500 WHERE entry IN (34141); 
+-- "Estruendor Invernal" --
+UPDATE creature_template SET mindmg=1705, maxdmg=2451 WHERE entry IN (34135);
+UPDATE creature_template SET mindmg=1645, maxdmg=2245 WHERE entry IN (34142);
+-- Guardianes de las puertas --
+UPDATE creature_template SET mindmg=2803, maxdmg=3941 WHERE entry IN (33722);
+UPDATE creature_template SET mindmg=2803, maxdmg=3941 WHERE entry IN (33723);
+-- Los 3 ancestros de freya: --
+-- Ancestro hojabrillante
+UPDATE creature_template SET mindmg=31411, maxdmg=41455 WHERE entry IN (33391);
+UPDATE creature_template SET mindmg=15470, maxdmg=18450 WHERE entry IN (32915); 
+-- Ancestro Hierrorrama --
+UPDATE creature_template SET mindmg=31411, maxdmg=41455 WHERE entry IN (33392);
+UPDATE creature_template SET mindmg=15470, maxdmg=18450 WHERE entry IN (32913);
+-- Ancestro Cortezapiedra --
+UPDATE creature_template SET mindmg=31411, maxdmg=41455 WHERE entry IN (33393); 
+UPDATE creature_template SET mindmg=15470, maxdmg=18450 WHERE entry IN (32914);
