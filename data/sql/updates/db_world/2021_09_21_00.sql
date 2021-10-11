c2bbd55b1bfef141b5666536f9adb1fb168c11be
arkane21 <arkane211@gmail.com>
2021-10-11 16:57:02 +0200

    Mal nombre corregido

--- data/sql/updates/db_world/2021_09_21_00.sql
+++ data/sql/updates/db_world/2021_09_21_00.sql
@@ -4,6 +4,6 @@ UPDATE spell_custom_attr SET attributes=12288 WHERE spell_id IN (64412);
 
 -- Aggro Druida Pollo/Gato --
 
-DELETE FROM spell_threath WHERE entry IN (24858,768);
+DELETE FROM spell_threat WHERE entry IN (24858,768);
 insert into `spell_threat` (`entry`, `flatMod`, `pctMod`, `apPctMod`) values('24858','0','1','0');
 insert into `spell_threat` (`entry`, `flatMod`, `pctMod`, `apPctMod`) values('768','0','1','0');
