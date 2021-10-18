3b430f1c2d514d9595524a647fdfbd621be0bab1
arkane21 <arkane211@gmail.com>
2021-10-18 17:17:28 +0200

    fix(DB) Loot faltante de Malygos 25m

---
+++ data/sql/updates/db_world/2021_10_18_00.sql
@@ -0,0 +1,2 @@
+delete from reference_loot_template where entry= 34175 and item=40549;
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('34175','40549','0','0','0','1','1','1','1',NULL);
