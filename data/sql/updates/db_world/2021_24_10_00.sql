08899168c6b7293e1033b267d6050d54ba8572f5
iyasenai <jh1160@hotmail.com>
2021-10-24 14:56:25 -0500

    Fix (DB) Inmunidades en los add thorim (1/2)

---
+++ data/sql/updates/db_world/2021_24_10_00.sql
@@ -0,0 +1,7 @@
+
+--  pleveyo
+UPDATE creature_template SET mechanic_immune_mask=617219967 WHERE entry IN (32904,33157);  
+-- belisario
+UPDATE creature_template SET mechanic_immune_mask=617219966 WHERE entry IN (32877,33155);  
+-- evocador
+UPDATE creature_template SET mechanic_immune_mask=617219967 WHERE entry IN (32878,33156);  
