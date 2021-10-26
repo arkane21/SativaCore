81ea74850e09f6a788ac7e16a6d8ab7de43d2489
iyasenai <jh1160@hotmail.com>
2021-10-26 13:10:15 -0500

    Desabilitar flecha congelante en ulduar

---
+++ data/sql/updates/db_world/2021_26_10_00.sql
@@ -0,0 +1,4 @@
+
+delete from disables where entry= 60192 and params_0= 603;
+insert into `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) values('0','60192','17','603','0','Desabilitar flecha congelante en Ulduar');
+
