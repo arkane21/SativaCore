171e8b3d5dd20b5e8a9d16cf6d420181337fd315
iyasenai <jh1160@hotmail.com>
2021-10-31 15:05:36 -0500

    Fix(DB) Ajuste de daño adds tajoscama (DeezWork)

---
+++ data/sql/updates/db_world/2021_31_10_00.sql
@@ -0,0 +1,9 @@
+
+-- vigia runa oscura 10j
+UPDATE creature_template SET mindmg=3400, maxdmg=4701 WHERE entry IN (33453); 
+-- vigia runa oscura 25j
+UPDATE creature_template SET mindmg=6800, maxdmg=9249 WHERE entry IN (33851); 
+-- runa oscura 10j
+UPDATE creature_template SET mindmg=4978, maxdmg=6471 WHERE entry IN (33388); 
+-- runa oscura 25j
+UPDATE creature_template SET mindmg=9010, maxdmg=12015 WHERE entry IN (33850); 
