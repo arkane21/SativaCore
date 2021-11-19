5a260c9e19c7ce8627e7ce5b0da70775ec7d5aa1
iyasenai <jh1160@hotmail.com>
2021-11-19 04:10:29 -0500

    Remaster(DB) nuevo Disenchant_loot_template

---
+++ data/sql/updates/db_world/2021_19_11_03.sql
@@ -0,0 +1,6 @@
+
+delete from disenchant_loot_template where entry= 70;
+insert into `disenchant_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `comment`) values('70','22450','0','15', '0', '1', '1', '1', '1', 'cristal del vacio');
+insert into `disenchant_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `comment`) values('70','34057','0','15', '0', '1', '1', '1', '1', 'cristal abisal');
+insert into `disenchant_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `comment`) values('70','34052','0','35', '0', '1', '1', '1', '1', 'fragmento onirico');
+insert into `disenchant_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `comment`) values('70','22449','0','35', '0', '1', '1', '1', '1', 'fragmento centallante grande');
