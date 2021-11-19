20c6a1aa3bb1f2d16c038800e534921f1b116cf3
iyasenai <jh1160@hotmail.com>
2021-11-19 04:40:34 -0500

    Remaster(DB) Murallas de fuego infernal 80

---
+++ data/sql/updates/db_world/2021_19_11_05.sql
@@ -0,0 +1,39 @@
+
+-- Hambriento mascahuesos
+UPDATE creature_template SET minlevel=80, maxlevel=80, mindmg=8951, maxdmg=11578, attackpower=805, HealthModifier=11.8 WHERE entry IN (18053); 
+-- Cuervoso mascahuesos
+UPDATE creature_template SET minlevel=80, maxlevel=80, mindmg=8951, maxdmg=11578, attackpower=805, HealthModifier=11.8 WHERE entry IN (18054);
+-- can de guerra mano destrozada
+UPDATE creature_template SET minlevel=80, maxlevel=80, mindmg=6120, maxdmg=8841, attackpower=805, HealthModifier=5 WHERE entry IN (18059);
+-- Taumaturgo oscuro foso sangrande
+UPDATE creature_template SET minlevel=80, maxlevel=80, mindmg=8951, maxdmg=11578, attackpower=805, HealthModifier=15, ManaModifier=5 WHERE entry IN (18049);
+-- destructor mascahuesos
+UPDATE creature_template SET minlevel=80, maxlevel=80, mindmg=8951, maxdmg=11578, attackpower=805, HealthModifier=14 WHERE entry IN (18052);
+-- arquero foso sangrante
+UPDATE creature_template SET minlevel=80, maxlevel=80, mindmg=8951, maxdmg=11578, attackpower=805, HealthModifier=12.8, ManaModifier=7 WHERE entry IN (18048);
+-- aruspice foso sangrante
+UPDATE creature_template SET minlevel=80, maxlevel=80, mindmg=8951, maxdmg=11578, attackpower=805, HealthModifier=12.8, ManaModifier=5 WHERE entry IN (18050);
+-- desgarrador mascahuesos
+UPDATE creature_template SET minlevel=80, maxlevel=80, mindmg=9951, maxdmg=12578, attackpower=805, HealthModifier=18 WHERE entry IN (18055);
+-- Maestro de bestias mascahuesos
+UPDATE creature_template SET minlevel=80, maxlevel=80, mindmg=8951, maxdmg=11578, attackpower=805, HealthModifier=14 WHERE entry IN (18051);
+
+-- vigia de fuego infernal
+UPDATE creature_template SET minlevel=80, maxlevel=80, mindmg=10548, maxdmg=13915, attackpower=805, HealthModifier=30, ManaModifier=10 WHERE entry IN (18058);
+-- boss guardian vigia Gargolma
+UPDATE creature_template SET minlevel=80, maxlevel=80, mindmg=15778, maxdmg=18487, attackpower=805, HealthModifier=165 WHERE entry IN (18436);
+
+-- boss omor el sinmarcas
+UPDATE creature_template SET minlevel=80, maxlevel=80, mindmg=15778, maxdmg=18487, attackpower=805, HealthModifier=136, ManaModifier=35 WHERE entry IN (18433);
+-- adds omor
+UPDATE creature_template SET minlevel=80, maxlevel=80, mindmg=4688, maxdmg=6571, attackpower=805, HealthModifier=10, ManaModifier=5 WHERE entry IN (18056);
+
+-- avizor de fuego infernal
+UPDATE creature_template SET minlevel=80, maxlevel=80, mindmg=10548, maxdmg=13915, attackpower=805, HealthModifier=29 WHERE entry IN (18057);
+-- Vazruden (montura)
+UPDATE creature_template SET minlevel=80, maxlevel=80, mindmg=15778, maxdmg=18487, attackpower=805, HealthModifier=135 WHERE entry IN (18435);
+-- boss Vazruden
+UPDATE creature_template SET minlevel=80, maxlevel=80, mindmg=15778, maxdmg=19487, attackpower=805, HealthModifier=135 WHERE entry IN (18434);
+-- boss nazan
+UPDATE creature_template SET minlevel=80, maxlevel=80, mindmg=18678, maxdmg=23516, attackpower=805, HealthModifier=152 WHERE entry IN (18432);
+
