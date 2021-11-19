587c28b62674fde2ea258135c1066eea7c2a4e39
iyasenai <jh1160@hotmail.com>
2021-11-19 04:12:34 -0500

    Remaster(DB) Reference_loot_template gemas epicas

---
+++ data/sql/updates/db_world/2021_19_11_04.sql
@@ -0,0 +1,82 @@
+
+delete from reference_loot_template where Entry= 190001;
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190001','40114','0','0','0','1','2','1','2','Gema Roja');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190001','40112','0','0','0','1','2','1','2','Gema Roja');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190001','40117','0','0','0','1','2','1','2','Gema Roja');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190001','40111','0','0','0','1','2','1','2','Gema Roja');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190001','40116','0','0','0','1','2','1','2','Gema Roja');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190001','40118','0','0','0','1','2','1','2','Gema Roja');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190001','40113','0','0','0','1','2','1','2','Gema Roja');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190001','40115','0','0','0','1','2','1','2','Gema Roja');
+
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190001','40126','0','0','0','1','2','1','2','Gema amarilla');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190001','40124','0','0','0','1','2','1','2','Gema amarilla');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190001','40123','0','0','0','1','2','1','2','Gema amarilla');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190001','40128','0','0','0','1','2','1','2','Gema amarilla');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190001','40125','0','0','0','1','2','1','2','Gema amarilla');
+
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190001','40120','0','0','0','1','2','1','2','Gema azul');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190001','40121','0','0','0','1','2','1','2','Gema azul');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190001','40119','0','0','0','1','2','1','2','Gema azul');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190001','40122','0','0','0','1','2','1','2','Gema azul');
+
+
+
+delete from reference_loot_template where Entry= 190002;
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40130','0','0','0','1','2','1','2','Gema morada');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40139','0','0','0','1','2','1','2','Gema morada');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40141','0','0','0','1','2','1','2','Gema morada');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40131','0','0','0','1','2','1','2','Gema morada');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40140','0','0','0','1','2','1','2','Gema morada');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40136','0','0','0','1','2','1','2','Gema morada');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40137','0','0','0','1','2','1','2','Gema morada');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40135','0','0','0','1','2','1','2','Gema morada');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40133','0','0','0','1','2','1','2','Gema morada');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40134','0','0','0','1','2','1','2','Gema morada');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40138','0','0','0','1','2','1','2','Gema morada');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40132','0','0','0','1','2','1','2','Gema morada');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40129','0','0','0','1','2','1','2','Gema morada');
+
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40159','0','0','0','1','2','1','2','Gema naranja');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40142','0','0','0','1','2','1','2','Gema naranja');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40160','0','0','0','1','2','1','2','Gema naranja');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40144','0','0','0','1','2','1','2','Gema naranja');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40161','0','0','0','1','2','1','2','Gema naranja');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40162','0','0','0','1','2','1','2','Gema naranja');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40148','0','0','0','1','2','1','2','Gema naranja');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40150','0','0','0','1','2','1','2','Gema naranja');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40154','0','0','0','1','2','1','2','Gema naranja');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40146','0','0','0','1','2','1','2','Gema naranja');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40143','0','0','0','1','2','1','2','Gema naranja');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40149','0','0','0','1','2','1','2','Gema naranja');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40151','0','0','0','1','2','1','2','Gema naranja');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40156','0','0','0','1','2','1','2','Gema naranja');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40147','0','0','0','1','2','1','2','Gema naranja');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40158','0','0','0','1','2','1','2','Gema naranja');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40157','0','0','0','1','2','1','2','Gema naranja');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40152','0','0','0','1','2','1','2','Gema naranja');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40145','0','0','0','1','2','1','2','Gema naranja');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40163','0','0','0','1','2','1','2','Gema naranja');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40155','0','0','0','1','2','1','2','Gema naranja');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40153','0','0','0','1','2','1','2','Gema naranja');
+
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40172','0','0','0','1','2','1','2','Gema verde');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40171','0','0','0','1','2','1','2','Gema verde');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40170','0','0','0','1','2','1','2','Gema verde');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40175','0','0','0','1','2','1','2','Gema verde');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40182','0','0','0','1','2','1','2','Gema verde');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40167','0','0','0','1','2','1','2','Gema verde');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40179','0','0','0','1','2','1','2','Gema verde');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40169','0','0','0','1','2','1','2','Gema verde');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40176','0','0','0','1','2','1','2','Gema verde');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40164','0','0','0','1','2','1','2','Gema verde');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40174','0','0','0','1','2','1','2','Gema verde');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40165','0','0','0','1','2','1','2','Gema verde');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40177','0','0','0','1','2','1','2','Gema verde');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40178','0','0','0','1','2','1','2','Gema verde');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40180','0','0','0','1','2','1','2','Gema verde');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40168','0','0','0','1','2','1','2','Gema verde');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40181','0','0','0','1','2','1','2','Gema verde');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40173','0','0','0','1','2','1','2','Gema verde');
+insert into `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) values('190002','40166','0','0','0','1','2','1','2','Gema verde');
+
